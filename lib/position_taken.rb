21c2028e4b0d7445fb961354d7290183550bbb6c
Alex Marz <alexhmarz@gmail.com>
2020-10-08 15:45:04 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if index == " " || "" || nil
+  if index == " " || index == "" || index == nil
     false
     elsif index == "X" || "O"
     true
